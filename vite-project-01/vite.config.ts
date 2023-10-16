439e272d2c527d177f7014ba219a2dec74b96c89
enderkc <enderhkc@gmail.com>
2023-10-16 17:02:01 +0800

    移动文件

---
+++ vite-project-01/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: '/absproxy/5173',
+  server: {
+    port: 5173,
+  },
+})
